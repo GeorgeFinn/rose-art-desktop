a79c7802c40b658a8323e88acf8315dacd9aab6c
GeorgeFinn <george.finn@icloud.com>
2017-05-04 23:00:21 -0400

    Fixed landing scree

--- RoseArt/src/app/-main-content/landing-screen/landing-screen.component.ts
+++ RoseArt/src/app/-main-content/landing-screen/landing-screen.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing-screen',
   templateUrl: './landing-screen.component.html',
-  styleUrls: ['./landing-screen.component.css']
+  styleUrls: ['./landing-screen.component.scss']
 })
 export class LandingScreenComponent implements OnInit {
 
