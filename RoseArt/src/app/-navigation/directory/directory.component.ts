2f2218db7f3f58a0ff379886dd27edc1c7707a4c
GeorgeFinn <george.finn@icloud.com>
2017-05-05 17:03:15 -0400

    work with ui

--- RoseArt/src/app/-navigation/directory/directory.component.ts
+++ RoseArt/src/app/-navigation/directory/directory.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { trigger, keyframes, animate, transition, style, NgZone, state } from '@angular/core';
 @Component({
   selector: 'app-directory',
   templateUrl: './directory.component.html',
-  styleUrls: ['./directory.component.css']
+  styleUrls: ['./directory.component.css'],
+  animations: [
+    trigger('wobble', [
+      transition('inactive => active', animate(1000, keyframes([
+        style({transform: 'translate3d(-20%, 0, 0) rotate3d(0, 0, 1, -3deg)', offset: .15}),
+        style({transform: 'translate3d(15%, 0, 0) rotate3d(0, 0, 1, 2deg)', offset: .30}),
+        style({transform: 'translate3d(-10%, 0, 0) rotate3d(0, 0, 1, -2deg)', offset: .45}),
+        style({transform: 'translate3d(5%, 0, 0) rotate3d(0, 0, 1, 1deg)', offset: .60}),
+        style({transform: 'translate3d(-5%, 0, 0) rotate3d(0, 0, 1, -1deg)', offset: .75}),
+        style({transform: 'none', offset: 1}),
+      ]))),
+    ])
+  ]
 })
 export class DirectoryComponent implements OnInit {
 
    public options = [1, 2, 3, 4, 5, 6, 7];
    public showHallsMenu: boolean = false;
+   public wobbleState: string;
 
-  constructor(router : Router) { }
+  constructor(router : Router, public zone: NgZone) { }
 
   ngOnInit() {
   }
@@ -44,5 +57,15 @@ export class DirectoryComponent implements OnInit {
 
   launchCrappo() {
     console.log("crappo");
+  }
+
+  triggerAnimation() {
+    this.wobbleState = "active";
+  }
+ 
+  reset() {
+    this.zone.run(() => {
+      this.wobbleState = "inactive";
+    });
   }
 }
