fb472fa7c914d65c012913b12024769ebf5a0a80
GeorgeFinn <george.finn@icloud.com>
2017-05-08 15:32:43 -0400

    sculptures page changes

--- RoseArt/src/app/halls/halls.component.ts
+++ RoseArt/src/app/halls/halls.component.ts
@@ -12,5 +12,6 @@ export class HallsComponent implements OnInit {
 
   ngOnInit() {
   }
+  
 
 }
--- RoseArt/src/app/photo-card/photo-card.component.ts
+++ RoseArt/src/app/photo-card/photo-card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-photo-card',
   templateUrl: './photo-card.component.html',
-  styleUrls: ['./photo-card.component.css']
+  styleUrls: ['./photo-card.component.scss']
 })
 export class PhotoCardComponent implements OnInit {
 
