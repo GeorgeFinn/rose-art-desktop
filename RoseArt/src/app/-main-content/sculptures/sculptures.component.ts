d24e806d6de2b6ccc72923791e9b2d485c99ebdf
Anthony M. De Rada <anthonyderada@gmail.com>
2017-05-08 02:39:21 -0400

    Started Sculptures page

--- RoseArt/src/app/-main-content/sculptures/sculptures.component.ts
+++ RoseArt/src/app/-main-content/sculptures/sculptures.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sculptures',
   templateUrl: './sculptures.component.html',
-  styleUrls: ['./sculptures.component.css']
+  styleUrls: ['./sculptures.component.scss']
 })
 export class SculpturesComponent implements OnInit {
+
+  public sculptures = [1, 2, 3];
 
   constructor() { }
 
