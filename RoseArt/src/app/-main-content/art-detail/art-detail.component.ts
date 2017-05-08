4c96fad64cb8d290fb1e95ad40284e8fa52f5d92
Anthony M. De Rada <anthonyderada@gmail.com>
2017-05-08 14:51:31 -0400

    Made images dynamic

--- RoseArt/src/app/-main-content/art-detail/art-detail.component.ts
+++ RoseArt/src/app/-main-content/art-detail/art-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'art-detail',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./art-detail.component.css']
 })
 export class ArtDetailComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() author: string;
+  @Input() imageUrl: string;
 
   constructor() { }
 
